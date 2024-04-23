0f9d19f3fcdb8196d052e7b803dae8c170a46099
Giovanny Moreno <giova1445@hotmail.com>
2024-04-22 20:15:39 -0500

    Install Laravel mix, Tailwindcss ☄️

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.json',
+    './templates/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
